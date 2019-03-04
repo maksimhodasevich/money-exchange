ab76fa9d9047b16cb9e053ef9626410ed94b8f2d
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2019-03-05 00:41:19 +0300

    alg of exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency > 10000){
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+    }
+
+    else if(currency > 0){
+        var answer = {
+            "H": 0,
+            "Q": 0,
+            "D": 0,
+            "N": 0,
+            "P": 0
+        }
+        answer.H = Math.trunc(currency/50);
+        currency = currency % 50;
+
+        answer.Q = Math.trunc(currency/25);
+        currency = currency % 25;
+
+        answer.D = Math.trunc(currency/10);
+        currency = currency % 10;
+
+        answer.N = Math.trunc(currency/5);
+
+        answer.P = currency % 5;
+
+        for (var prop in answer) {
+            if(answer[prop] == 0){
+                delete answer[prop];
+            }
+        }
+        return answer;   
+    }
+    else {
+        return {};
+    }
+
 }
+
